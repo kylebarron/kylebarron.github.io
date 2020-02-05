b63d7b1df66e832df5a9459b5f008327a598c784
Kyle Barron <barronk@mit.edu>
2020-02-05 12:18:36 -0500

    Customize site settings in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Kyle Barron`,
   },
   plugins: [
     {
@@ -22,12 +22,8 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: 'Github',
+            url: 'https://github.com/kylebarron',
           },
         ],
       },
@@ -39,30 +35,30 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+    //     short_name: `minimal-blog`,
+    //     description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+    //     start_url: `/`,
+    //     background_color: `#fff`,
+    //     theme_color: `#6B46C1`,
+    //     display: `standalone`,
+    //     icons: [
+    //       {
+    //         src: `/android-chrome-192x192.png`,
+    //         sizes: `192x192`,
+    //         type: `image/png`,
+    //       },
+    //       {
+    //         src: `/android-chrome-512x512.png`,
+    //         sizes: `512x512`,
+    //         type: `image/png`,
+    //       },
+    //     ],
+    //   },
+    // },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
